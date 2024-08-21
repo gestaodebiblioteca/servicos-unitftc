532875f5211c15443101e1a5a4ae84f48b0cdfb5
gestaodebiblioteca <gestaodebiblioteca@ftc.edu.br>
2024-08-21 20:18:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import streamlit as st
-<<<<<<< HEAD
 import pandas as pd
-import plotly.express as px # type: ignore
-=======
+import plotly.express as px  # type: ignore
 from pages.minha_biblioteca import minha_biblioteca
 from pages.ebsco import page_ebsco
->>>>>>> 2d5a6d2 (Separendo por pastas)
 
 # Configurar a página
 st.set_page_config(layout="wide")
@@ -20,11 +17,8 @@ if page == "Minha Biblioteca":
     minha_biblioteca()
 elif page == "EBSCO":
     page_ebsco()
-<<<<<<< HEAD
     # Botões para mostrar e esconder dados
     st.sidebar.button("Mostrar Dados", on_click=toggle_data_visibility)
     #if st.session_state.show_data:
         #st.write(df_detalhado)
         #st.sidebar.button("Esconder Dados", on_click=toggle_data_visibility)
-=======
->>>>>>> 2d5a6d2 (Separendo por pastas)
